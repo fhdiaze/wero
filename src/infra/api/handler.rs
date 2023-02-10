594f8fc656326ebb4badc5db573512a8c3ce3954
Fredy Diaz <Fredy.Diaz@vocusgroup.co.nz>
2023-02-10 17:08:54 +1300

    feat: improve error management

--- src/infra/api/handler.rs
+++ src/infra/api/handler.rs
@@ -2,11 +2,12 @@ use super::problem::Problem;
 use crate::infra::{api::problem, error::AppError};
 
 pub fn handle(error: &AppError) -> Problem {
+    let default_message = "An unexpected error occurred while processing the request";
     tracing::error!("{}", error);
     match error {
         AppError::NotFound(_) => Problem::from_kind(problem::Kind::NotFound, error.to_string()),
         AppError::Validation(_) => Problem::from_kind(problem::Kind::BadRequest, error.to_string()),
-        AppError::Mongo(err) => Problem::from_kind(problem::Kind::InternalServerError, err.to_string()),
-        AppError::Config(err) => Problem::from_kind(problem::Kind::InternalServerError, err.to_string()),
+        AppError::Mongo(_) => Problem::from_kind(problem::Kind::InternalServerError, String::from(default_message)),
+        AppError::Config(_) => Problem::from_kind(problem::Kind::InternalServerError, String::from(default_message)),
     }
 }
