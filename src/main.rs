8639693e2773e866aefff5e973012415d96f01a4
Fredy Diaz <Fredy.Diaz@vocusgroup.co.nz>
2023-02-19 19:45:13 +1300

    feat: add logging for req/resp

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,15 @@ use std::{net::SocketAddr, sync::Arc};
 use tower::ServiceBuilder;
 use tower_http::{
   cors::{Any, CorsLayer},
-  trace::TraceLayer,
+  trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
 };
+use tracing::Level;
 
 mod domain;
 mod infra;
 mod modules;
 
-fn route() -> Router<DynDbClient> {
+fn router() -> Router<DynDbClient> {
   let races = race::controller::route();
   Router::new().nest("/api", races)
 }
@@ -38,9 +39,12 @@ async fn main() {
   let cors = CorsLayer::new()
     .allow_methods([Method::GET, Method::POST])
     .allow_origin(Any);
-  let trace = TraceLayer::new_for_http();
+  let trace = TraceLayer::new_for_http()
+    .make_span_with(DefaultMakeSpan::new().include_headers(true))
+    .on_request(DefaultOnRequest::new().level(Level::INFO))
+    .on_response(DefaultOnResponse::new().level(Level::INFO));
   let service_builder = ServiceBuilder::new().layer(trace).layer(cors);
-  let router = route().layer(service_builder).with_state(db_client);
+  let router = router().layer(service_builder).with_state(db_client);
   let addr = SocketAddr::from(([0, 0, 0, 0], config.server.port));
 
   tracing::info!("Server is listening on address={}", addr);
